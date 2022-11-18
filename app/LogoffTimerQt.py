4d6c1c7b8bac1acb0239040e1083f0b562542188
René Peschmann <peschmann@gmx.de>
2022-11-18 19:27:21 +0100

    dial update

--- app/LogoffTimerQt.py
+++ app/LogoffTimerQt.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from PyQt6 import uic
@@ -8,7 +9,8 @@ from app.uiInterfaces.LogoffTimerUi import LogoffTimerUi
 
 class LogoffTimerQt(QWidget):
     ui: LogoffTimerUi
-    timer = None
+    shutdown_after = None
+    want_shutdown_after = None
 
     def __init__(self, root_dir, parent=None):
         super(LogoffTimerQt, self).__init__(parent)
@@ -20,6 +22,18 @@ class LogoffTimerQt(QWidget):
     def __setup(self):
         self.ui.btn_set_timer.clicked.connect(self.logoff_timer_set)
         self.ui.btn_cancel_timer.clicked.connect(self.logoff_timer_cancel)
+        self.ui.logoff_dial.valueChanged.connect(self.dial_value_changed)
+
+    def get_minutes(self, value=None):
+        if value is None:
+            value = self.ui.logoff_dial.value()
+        return value * 10
+
+    def dial_value_changed(self, value):
+        minutes_in_future = self.get_minutes(value)
+        self.ui.logoff_ic.display(minutes_in_future)
+        self.want_shutdown_after = datetime.datetime.now
+        self.want_shutdown_after = datetime.timedelta(seconds=minutes_in_future*60)
 
     def show(self):
         self.ui.show()
@@ -28,8 +42,7 @@ class LogoffTimerQt(QWidget):
         self.ui.hide()
 
     def logoff_timer_set(self):
-        print('foo')
+        self.shutdown_after = self.want_shutdown_after
 
     def logoff_timer_cancel(self):
-        self.timer = None
-        print('bar')
+        self.shutdown_after = None
