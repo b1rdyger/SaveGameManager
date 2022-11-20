d24a509d0a4b8d23857450af8904c32eeb831832
René Peschmann <peschmann@gmx.de>
2022-11-20 20:25:52 +0100

    new config format

--- app/ConfigController.py
+++ app/ConfigController.py
@@ -7,48 +7,46 @@ from app.CheckRegedit import CheckRegedit, GameInfo
 
 
 class ConfigController:
-    config: dict
+    config: json
 
-    def __init__(self, root_dir, engine):
+    def __init__(self, root_dir):
         self.config_file = f'{root_dir}config{os.sep}games.json'
-        self.engine = engine
-        if self.__check_configfile(self.config_file):
-            self.__load_config()
+        if not self.__load_config(self.config_file):
+            raise FileNotFoundError('Config file could not be loaded')
+
         check_regedit = CheckRegedit()
-        run_cmd = self.config.get('run_cmd')
+        for profile in self.config.get('profiles'):
+            self.__fix_directories(profile)
+            self.__fix_run_cmd(profile, check_regedit)
+
+    def __getitem__(self, profile, item):
+        return self.config.get(profile).get(item)
+
+    def __load_config(self, file) -> bool:
+        if bool(os.path.exists(file)):
+            with open(self.config_file, 'r') as read_content:
+                self.config = json.load(read_content)
+                return True
+        return False
+
+    def __fix_run_cmd(self, sub_config, regedit):
+        sub_item = self.config.get('profiles').get(sub_config)
+        run_cmd = sub_item.get('run_cmd')
         if run_id_match := re.search('^id:([0-9]+)$', run_cmd):
             run_id = run_id_match[1]
-            if run_id in check_regedit.available_games:
-                game_info: GameInfo = check_regedit.available_games[run_id]
-                self.config['run_cmd'] = game_info.run_cmd
+            if run_id in regedit.available_games:
+                game_info: GameInfo = regedit.available_games[run_id]
+                sub_item['run_cmd'] = game_info.run_cmd
 
-    def __getitem__(self, item):
-        return self.config.get(item)
-
-    @staticmethod
-    def __check_configfile(file):
-        return bool(os.path.exists(file))
-
-    def __load_config(self):
-        with open(self.config_file, 'r') as read_content:
-            self.config = json.load(read_content)
-            self.config['common_save_dir'] = self.__get_real_path(self.config['common_save_dir'])
-        for one_backup_folder in self.config['backup_save_dirs']:
+    def __fix_directories(self, sub_config):
+        sub_item = self.config.get('profiles').get(sub_config)
+        sub_item['common_save_dir'] = self.__get_real_path(sub_item['common_save_dir'])
+        for one_backup_folder in sub_item['backup_save_dirs']:
             one_backup_folder['location'] = self.__get_real_path(one_backup_folder['location'])
-        for item in self.config.items():
-            if item[0].startswith('_'):
-                print('Item Ignored')
-                continue
-            else:
-                for value in enumerate(item):
-                    print(value)
 
     # noinspection PyMethodMayBeStatic
     def __get_real_path(self, path):
         if os.name == 'nt' and '%USERPROFILE%' in path:
             new_path = os.path.expanduser(os.environ['USERPROFILE'])
-            # logger.info(f'{path.replace("%USERPROFILE%", new_path)}')
             return path.replace("%USERPROFILE%", new_path)
         return path
-
-
